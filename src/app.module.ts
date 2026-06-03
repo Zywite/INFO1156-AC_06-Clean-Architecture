9099177c1531789fa49556c4f6b5b34410163d40
frpeo <franciscolarenas4@gmail.com>
2026-06-03 17:10:09 -0400

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,12 @@
 import { Module } from "@nestjs/common"
-import { CategoriesModule } from "@/categories/categories.module"
-import { CommentsModule } from "@/comments/comments.module"
-import { LikesModule } from "@/likes/likes.module"
-import { ModerationModule } from "@/moderation/moderation.module"
-import { PostsModule } from "@/posts/posts.module"
-import { PrismaModule } from "@/shared/prisma.module"
+import { PrismaModule } from "@/infrastructure/persistence/prisma.module"
+import { PostsModule } from "@/infrastructure/http/posts/posts.module"
+import { CommentsModule } from "@/infrastructure/http/comments/comments.module"
+import { LikesModule } from "@/infrastructure/http/likes/likes.module"
+import { CategoriesModule } from "@/infrastructure/http/categories/categories.module"
+import { ModerationModule } from "@/infrastructure/http/moderation/moderation.module"
 
 @Module({
-    imports: [
-        PrismaModule,
-        CategoriesModule,
-        PostsModule,
-        CommentsModule,
-        LikesModule,
-        ModerationModule,
-    ],
+    imports: [PrismaModule, PostsModule, CommentsModule, LikesModule, CategoriesModule, ModerationModule],
 })
 export class AppModule {}
