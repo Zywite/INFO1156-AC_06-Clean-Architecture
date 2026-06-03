42cdc3537bec4cc24beeecf25729a0a61eef17a5
Benjitapley <benjaminbarra090@gmail.com>
2026-06-03 18:51:49 -0400

    chore: remove old monolithic modules (posts, comments, likes, categories, moderation)

--- src/categories/categories.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common"
-import { CategoriesController } from "@/categories/categories.controller"
-import { CategoriesService } from "@/categories/categories.service"
-
-@Module({
-    controllers: [CategoriesController],
-    providers: [CategoriesService],
-})
-export class CategoriesModule {}
