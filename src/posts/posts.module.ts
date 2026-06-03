8e220c1840558095d6a7fac3663c652c904dca24
frpeo <franciscolarenas4@gmail.com>
2026-06-03 17:08:51 -0400

    a
    xd

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,13 +1,13 @@
 import { Module } from "@nestjs/common"
-import { FeedRankingStrategyFactory } from "@/posts/feed-ranking.strategy"
-import { ModerationModule } from "@/moderation/moderation.module"
-import { PostsController } from "@/posts/posts.controller"
-import { PostsService } from "@/posts/posts.service"
+import { PostsController } from "./posts.controller"
+import { CreatePostUseCase } from "@/application/use-cases/posts/create-post.use-case"
+import { GetFeedUseCase } from "@/application/use-cases/posts/get-feed.use-case"
+import { GetPostsUseCase } from "@/application/use-cases/posts/get-posts.use-case"
+import { ModerationDomainService } from "@/domain/services/moderation.service"
+import { FeedRankingStrategyFactory } from "@/domain/services/feed-ranking.strategy"
 
 @Module({
-    imports: [ModerationModule],
     controllers: [PostsController],
-    providers: [PostsService, FeedRankingStrategyFactory],
-    exports: [PostsService],
+    providers: [CreatePostUseCase, GetFeedUseCase, GetPostsUseCase, ModerationDomainService, FeedRankingStrategyFactory],
 })
 export class PostsModule {}
